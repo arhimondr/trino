6848effc42a4f1003e47ff5407911c99eda65b9e
Andrii Rosa <andriyrosa@gmail.com>
2021-10-28 13:40:18 -0400

    Integrate external exchange with exchange operator

--- core/trino-main/src/main/java/io/trino/testing/LocalQueryRunner.java
+++ core/trino-main/src/main/java/io/trino/testing/LocalQueryRunner.java
@@ -857,7 +857,8 @@ public class LocalQueryRunner
                 new DynamicFilterConfig(),
                 typeOperators,
                 blockTypeOperators,
-                tableExecuteContextManager);
+                tableExecuteContextManager,
+                exchangeManagerRegistry);
 
         // plan query
         StageExecutionDescriptor stageExecutionDescriptor = subplan.getFragment().getStageExecutionDescriptor();
